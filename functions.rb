d6d37840a9f5d08db8662325d2efa1bb902e1437
bavan <thihal123@gmail.com>
2014-08-06 17:22:00 +0100

    added check_status function

--- functions.rb
+++ functions.rb
@@ -57,9 +57,12 @@ def get_system_type
  return f.split[0..1].join " "
 end
 
-def uptime
- f = `uptime`
- return f.gsub(/\s/, "%20")
+def uptime                                             
+    f = `uptime`                                       
+    uptime = f.gsub(" ", "%20").gsub("\n","")          
+    seconds = `cat /proc/uptime`                       
+    seconds = seconds.split(' ').first.gsub("\n","")   
+    return "#{uptime}, #{seconds}"                     
 end
 
 def get_serial
@@ -112,6 +115,35 @@ def get_prefs
     pref.gsub("\n","") 
    end
 end
+
+def ca_checksum                                                                                                                   
+  file = "/etc/openvpn/ca.crt"                                                                                                    
+  if File.file?(file)                                                                                                             
+    cksum = `openssl md5 /etc/openvpn/ca.crt | sed "s/^.*= *//"`                                                                  
+    cksum.gsub("\n","")                                                                                                           
+  end                                                                                                                             
+end
+
+def get_icmp_response(ip)
+ `ping  -c 1 #{ip}`
+ if $? == 0
+   return success
+ else   
+  return failure
+ end      
+end 
+ 
+def log_interface_change
+  `dmesg | awk -F ] '{"cat /proc/uptime | cut -d \" \" -f 1" | getline st;a=substr( $1,2,length($1) - 1);print strftime("%F %H:%M:%S %Z",systime()-st+a)" -> "$0}' | grep  eth1 | tail -1 | awk ' { print $all  }' >> /etc/status`
+end  
+ 
+def log_lost_ip
+  `echo `date` lost ip address  >> /etc/status`  
+end 
+ 
+def log_gateway_failure
+  `echo `date` Cannot ping to gateway >> /etc/status`
+end  
  
 def iwinfo
   `sh /etc/scripts/iwinfo.sh #{get_active_wlan}`
--- gubbins.rb
+++ gubbins.rb
@@ -7,24 +7,75 @@ require '/etc/scripts/functions.rb'
 require 'zlib'
 
 class GoGoGubbins
+  
+  def api_url                                        
+      "https://api.polkaspots.com"                     
+  end
+
+  def health_url                                     
+      "http://health.polkaspots.com/api/v1/health"     
+  end
 
   def check_connectivity_and_heartbeat
     system 'touch /tmp/gubbins.lock'
     response = check_success_url
     check_lights
-    if response == "200"
+    if ["200", "201", "404", "500"].include? response 
       run_heartbeat
     else
       i=0
       loop do
         i+=1
-        set_ssid_state
+        check_status(i)
         sleep 15
-        break print "I'm alive" if $live == true
+       if $live == true
+         change_back
+         break
       end
     end
   end
+  
+  def externel_server
+    return "8.8.8.8"
+  end  
+  
+  def check_status(change)
+    @wan_status = wan_status
+    if (@wan_status != "up")
+      if(change == 2)
+       log_interface_change
+      end
+      check_status(1) 
+      else 
+      @wan_ip = get_wan_ip(get_wan_name)
+      wan_response = get_icmp_response(@wan_ip)
+      if (result != "success" )
+        if (change == 2)
+         log_lost_ip
+        end
+        else
+          @gateway = gateway
+          gw_response = get_icmp_response(@gateway)
+          if(gw_response != "success")
+            if(change == 2)
+             log_gateway_failure
+            end
+            else
+               es_response = get_icmp_response(externel_server)
+               if (es_response != "success")
+                 if (change == 2)
+                 system`kilall chilli && cp /etc/chilli/default /etc/chilli/online  && cp /etc/chilli/no_internet /etc/chilli/defaults && /etc/init.d/chilli restart`   
+                 end
+              else 
+                $live  = true 
+                return $live
+                end     
+             end     
+          end 
+      end 
+   end       
 
+          
   def check_lights
     @sync = get_sync
     response = check_success_url
@@ -77,23 +128,24 @@ class GoGoGubbins
     @iwdump = iwdump
     @firmware = firmware
     @scan = airodump
+    @ca = ca_checksum
     @prefs= get_prefs
     data = "{\"data\":{\"serial\":\"#{$serial}\",\"ip\":\"#{@tun_ip}\",\"lmac\":\"#{$lan_mac}\",\"system\":\"#{$system_type}\",\"machine_type\":\"#{$machine_type}\",\"firmware\":\"#{@firmware}\",\"wan_interface\":\"#{$wan_name}\",\"wan_ip\":\"#{get_wan_ip($wan_name)}\",\"uptime\":\"#{uptime}\",\"sync\":\"#{@sync}\",\"version\":\"#{$version}\",\"chilli\":\"#{chilli_list}\",\"prefs\":\"#{@prefs}\",\"iwinfo\":#{@iwinfo},\"iwdump\":#{@iwdump}}"
       Zlib::GzipWriter.open('/tmp/data.gz') do |gz|
       gz.write data
       gz.close
     end
-    system("curl --silent --connect-timeout 5 -F data=@/tmp/data.gz -F 'mac=#{$wan_mac}' https://api.polkaspots.com/api/v1/nas/gubbins -k | ash")
+    system("curl --silent --connect-timeout 5 -F data=@/tmp/data.gz -F 'mac=#{$wan_mac}' -F 'ca=#{@ca}' #{api_url}/api/v1/nas/gubbins -k | ash")
     system 'rm -rf /tmp/gubbins.lock'
   end
 
-  def check_success_url
-    %x{curl --connect-timeout 5 --write-out "%{http_code}" --silent --output /dev/null "http://www.bbc.co.uk/"}
+  def check_success_url                                                                                                                                
+      %x{curl --connect-timeout 5 --write-out "%{http_code}" --silent --output /dev/null "#{health_url}"}                                                
   end
 
 end
 
-if File.exists?('/tmp/gubbins.lock') && File.ctime('/tmp/gubbins.lock') > (Time.now - 300)
+if File.exists?('/tmp/gubbins.lock') && File.ctime('/tmp/gubbins.lock') > (Time.now - 60)
   puts "Already testing the connectivity"
 else
   GoGoGubbins.new.check_connectivity_and_heartbeat
